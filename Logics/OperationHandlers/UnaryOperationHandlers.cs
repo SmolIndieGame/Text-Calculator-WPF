bc2fd50da492f978c0afffeca31f17f6d0b713e2
SmolIndieGame <smolindiegame@gmail.com>
2022-10-06 21:02:19 +0800

    made some unary operation handlers

--- Logics/OperationHandlers/UnaryOperationHandlers.cs
+++ Logics/OperationHandlers/UnaryOperationHandlers.cs
@@ -43,6 +43,48 @@ namespace Text_Caculator_WPF
         public override OperationResult Calculate(double a) => Math.Sin(a);
     }
 
+    public sealed class CosineHandler : BaseUnaryOperationHandler
+    {
+        public override string Symbol { get; } = "cos";
+
+        public override OperationResult Calculate(double a) => Math.Cos(a);
+    }
+
+    public sealed class TangentHandler : BaseUnaryOperationHandler
+    {
+        public override string Symbol { get; } = "tan";
+
+        public override OperationResult Calculate(double a) => Math.Tan(a);
+    }
+
+    public sealed class LogarithmHandler : BaseUnaryOperationHandler
+    {
+        public override string Symbol { get; } = "log";
+
+        public override OperationResult Calculate(double a) => Math.Log10(a);
+    }
+
+    public sealed class NaturalLogHandler : BaseUnaryOperationHandler
+    {
+        public override string Symbol { get; } = "ln";
+
+        public override OperationResult Calculate(double a) => Math.Log(a);
+    }
+
+    public sealed class SquareRootHandler : BaseUnaryOperationHandler
+    {
+        public override string Symbol { get; } = "sqrt";
+
+        public override OperationResult Calculate(double a) => Math.Sqrt(a);
+    }
+
+    public sealed class CubeRootHandler : BaseUnaryOperationHandler
+    {
+        public override string Symbol { get; } = "cbrt";
+
+        public override OperationResult Calculate(double a) => Math.Cbrt(a);
+    }
+
     [ParseInstantly]
     public sealed class FactorialHandler : BaseUnaryOperationHandler
     {
